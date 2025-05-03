0a7e538696f6a510a8dd5133c38238ca3ca48249
aki75365 <L2007527327as@gmail.com>
2025-05-03 09:11:43 +0900

    kadai26

--- src/test/java/raisetech/Student/Management/StudentServiceTest.java
+++ src/test/java/raisetech/Student/Management/StudentServiceTest.java
@@ -11,8 +11,15 @@ import raisetech.Student.Management.data.Student;
 import raisetech.Student.Management.repository.StudentRepository;
 import raisetech.Student.Management.service.StudentService;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.eq;
+
 
 public class StudentServiceTest {
 
