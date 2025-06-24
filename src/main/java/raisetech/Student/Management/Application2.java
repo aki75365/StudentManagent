90440bcc4cdb03029ea48142c118f9962c043c84
aki75365 <L2007527327as@gmail.com>
2025-06-24 14:26:52 +0900

    アドバンス４回 戻り値の型の修正

--- src/main/java/raisetech/Student/Management/Application2.java
+++ src/main/java/raisetech/Student/Management/Application2.java
@@ -1,10 +1,15 @@
 package raisetech.Student.Management;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import raisetech.Student.Management.repository.StudentRepository2;
+import raisetech.Student.Management.data.Student;  // ここを追加
 
 @SpringBootApplication
 @RestController
@@ -17,7 +22,15 @@ public class Application2 {
 		SpringApplication.run(Application2.class, args);
 	}
 
+	// 全受講生リストを取得するエンドポイント
+	@GetMapping("/studentList")
+	public List<Student> getStudentList() {
+		return repository.findAllStudents();
+	}
 
-
-
+	// 全受講生リストを取得するエンドポイント（例）
+	@GetMapping("/student_course")
+	public List<Student> getStudentCourse() {
+		return repository.findAllStudents();
+	}
 }
